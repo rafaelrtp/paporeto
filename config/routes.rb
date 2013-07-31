fd16f4dabaae446e513c118e7caf0fe02f9cb457
Rafael Torales <rafaeltorales@new-host-53.home>
2013-07-31 20:49:13 -0300

    tirando sign_up e permitindo atualizar nome ou email sem alterar senha

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Paporeto::Application.routes.draw do
   devise_for :users
 
+  resources :users
   resources :categories
-
   resources :articles, path: 'artigos', path_names: {new: 'novo', edit: 'editar'}
   root 'articles#index'
 
