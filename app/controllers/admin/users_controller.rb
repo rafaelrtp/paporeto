0c2702dac1c9227cbcf64e916112ef09282d249a
Rafael Torales <rafaeltorales@new-host-53.home>
2013-07-31 23:12:25 -0300

    colocando arquivos na pasta admin

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,4 +1,4 @@
-class UsersController < ApplicationController
+class Admin::UsersController < ApplicationController
   before_action :authenticate_user!
   before_action :set_user, only: [:show, :edit, :update, :destroy]
 
@@ -29,7 +29,7 @@ class UsersController < ApplicationController
 
     respond_to do |format|
       if @user.save
-        format.html { redirect_to @user, notice: 'User was successfully created.' }
+        format.html { redirect_to [:admin, @user], notice: 'User was successfully created.' }
         format.json { render action: 'show', status: :created, location: @user }
       else
         format.html { render action: 'new' }
@@ -53,7 +53,7 @@ class UsersController < ApplicationController
           sign_in(@user, bypass: true)
         end
 
-        format.html { redirect_to @user, notice: 'User was successfully updated.' }
+        format.html { redirect_to [:admin, @user], notice: 'User was successfully updated.' }
         format.json { head :no_content }
       else
         format.html { render action: 'edit' }
@@ -67,7 +67,7 @@ class UsersController < ApplicationController
   def destroy
     @user.destroy
     respond_to do |format|
-      format.html { redirect_to users_url }
+      format.html { redirect_to admin_users_url }
       format.json { head :no_content }
     end
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Paporeto::Application.routes.draw do
   devise_for :users
 
-  resources :users
-  resources :categories
-  resources :articles, path: 'artigos', path_names: {new: 'novo', edit: 'editar'}
-  root 'articles#index'
+  namespace :admin do
+    resources :users
+    resources :categories
+    resources :articles, path: 'artigos', path_names: {new: 'novo', edit: 'editar'}
+  end
+
+  root 'admin/articles#index'
 
   mount Uploadbox::Engine => '/uploadbox', as: :uploadbox
 
